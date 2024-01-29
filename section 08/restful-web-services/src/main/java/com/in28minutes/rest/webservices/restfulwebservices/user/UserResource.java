1091c20f84969c5852c4b8299394ba3bd2163944
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-29 17:44:24 +0900

    섹션 08. 149. Step 08 - Implementing GET Methods for User Resource : GET - findAll, findOne

---
+++ section 08/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,34 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * author : ms.Lee
+ * date   : 2024-01-29
+ */
+@RestController
+public class UserResource {
+
+  private UserDaoService service;
+
+  public UserResource(UserDaoService service) {
+
+    this.service = service;
+  }
+
+  @GetMapping (path = "/users")
+  public List<User> retrieveAllUsers() {
+
+    return service.findAll();
+  }
+
+  @GetMapping (path = "/users/{id}")
+  public User retrieveUser(@PathVariable int id) {
+
+    return service.findOne( id );
+  }
+}
