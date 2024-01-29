f76b57ed8ad4ba0a175cb0b049c573f6b51dc196
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-29 16:06:40 +0900

    섹션 08. 142. Step 02 - Creating a Hello World REST API with Spring Boot : RequestMapping or GetMapping

---
+++ section 08/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author : ms.Lee
+ * date   : 2024-01-29
+ */
+// 1. REST API
+@RestController
+public class HelloWorldController {
+
+  // 2. /helloworld
+
+  // return "Hello World"
+
+  // --> path로 해도 되네??
+  // @RequestMapping(method = RequestMethod.GET, path = "/hello-world") --> 아래처럼
+  @GetMapping (path = "/hello-world")
+  public String helloWorld() {
+
+    return "Hello World";
+  }
+}
