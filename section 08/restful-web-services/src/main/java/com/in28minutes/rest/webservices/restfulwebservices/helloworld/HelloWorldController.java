6fb338433a3aed8f99a1350823a5758826492895
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-29 17:05:02 +0900

    섹션 08. 145. Step 05 - Enhancing the Hello World REST API with a Path Varialbe : name 을 같게 설정한 예제

--- section 08/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ section 08/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,6 +27,17 @@ public class HelloWorldController {
   @GetMapping (path = "/hello-world-bean")
   public HelloWorldBean helloWorldBean() {
 
-    return new HelloWorldBean("Hello World");
+    return new HelloWorldBean( "Hello World" );
+  }
+
+  // Path Parameters
+  // /users/{id}/todos/{id} =>/users/1/todos/101
+  // /hello-world/path-variable/{name}
+  @GetMapping (path = "/hello-world/path-variable/{name}")
+  public HelloWorldBean helloWorldBeanVariable(@PathVariable String name) {
+
+    return new HelloWorldBean(
+        String.format( "Hello World, %s", name )
+    );
   }
 }
