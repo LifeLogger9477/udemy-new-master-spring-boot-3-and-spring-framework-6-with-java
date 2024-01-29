41e455dd218bf27776514316084e5ad63e3aa3ba
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-29 16:41:12 +0900

    섹션 08. 143. Step 03 - Enhancing the Hello World REST API to return a Bean : class to return json

--- section 08/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ section 08/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -22,4 +22,10 @@ public class HelloWorldController {
 
     return "Hello World";
   }
+
+  @GetMapping (path = "/hello-world-bean")
+  public HelloWorldBean helloWorldBean() {
+
+    return new HelloWorldBean("Hello World");
+  }
 }
